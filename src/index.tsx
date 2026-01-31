a72fa079f5f94d2d48a589baf0db9be38061672b
dreamforjesus1006-hue <dreamforjesus1006@gmail.com>
2026-01-31 15:28:11 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // 這裡就是呼叫剛剛建立的 css
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
