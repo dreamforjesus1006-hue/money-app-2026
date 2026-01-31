94458f4174bead0e5cba14132d5d43744ec69570
dreamforjesus1006-hue <dreamforjesus1006@gmail.com>
2026-01-31 15:21:38 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // 這裡就是呼叫剛剛建立的 css
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
+  </React.StrictMode>
 );
